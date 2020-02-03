89822c89027e2a6c698afbcc55b25dcca370343d
ALU1J <ALU1J@PC306>
2020-02-03 10:24:16 +0100

    version comentada

--- geometria/src/Rectangulo_dtb.java
+++ geometria/src/Rectangulo_dtb.java
@@ -1,20 +1,33 @@
 
 public class Rectangulo_dtb extends FiguraGeometrica_dtb {
+	/**
+	 * autor:Daniel Terol Barbero, v:1.2, 
+	 */
 	private double l1;
 	private double l2;
 	
+	/**
+	 * creador de la clase rectangulo.
+	 * @param tipoFigura tipo de la figura.
+	 * @param lG un lado.
+	 * @param lP otro lado.
+	 */
 	public Rectangulo_dtb(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
-	@Override
+	/**
+	 * calcula el area.
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
-	@Override
+	/**
+	 * calcula el perimetro.
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
